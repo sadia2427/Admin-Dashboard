66e80a43e015136189d8230b26c4a513368d1962
SADIA <fahmidasadia94@gmail.com>
2022-09-17 20:56:31 +0600

    Project Files

---
+++ orders.js
@@ -0,0 +1,44 @@
+const Orders = [
+    {
+        productName: 'Red Hearted Pendent',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Sky Blue Color Tops',
+        productNumber: '36378',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Black DT102 Smartwatch',
+        productNumber: '49347',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Calvin Klein Ladies Bag',
+        productNumber: '96996',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Blue Lady Perfume',
+        productNumber: '22821',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Black Polo T-Shirt',
+        productNumber: '81475',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Yellow Band Neck Shirt',
+        productNumber: '22821',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    }
+]
